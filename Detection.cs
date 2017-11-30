94b901c3cded9994a001cac907bf0478747bbd8d
BlockDMask <blockdmask@gmail.com>
2017-12-01 03:01:53 +0900

    v0.1
    보완해야할 사항이 많음.
    지금 구현된것.
    -> 주인공 인식 (디버그 로그 찍히게 함)
    -> 주인공 인식 후 주인공 바라봄. 
    
    구현 해야할 것.
    -> 주인공의 상태를 받아서 처리해야함.
    -> 얘 왜 누워서 바라보지?

---
+++ Detection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){	//player detection.
+		Debug.Log ("detection : " + other.gameObject);
+		this.transform.LookAt (other.transform);	//player를 향해 고개를 돌림.
+		//if(other.gameObject.state == normal) -> gameover() 이런식이 될듯.
+
+
+	}
+
+}
