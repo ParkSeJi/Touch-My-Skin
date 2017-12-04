f4f73e5ed0e6d474b07a4a9daa33d7f5d0d69937
BlockDMask <blockdmask@gmail.com>
2017-12-05 03:29:54 +0900

    Add files via upload

--- Detection.cs
+++ Detection.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Detection : MonoBehaviour {
+	public PlayerState ps = PlayerState.normal;
+	public int mon_state;
 
 	// Use this for initialization
 	void Start () {
-		
+		mon_state = -1;
 	}
 	
 	// Update is called once per frame
@@ -17,9 +19,20 @@ public class Detection : MonoBehaviour {
 	void OnTriggerEnter(Collider other){	//player detection.
 		Debug.Log ("detection : " + other.gameObject);
 		this.transform.LookAt (other.transform);	//player를 향해 고개를 돌림.
-		//if(other.gameObject.state == normal) -> gameover() 이런식이 될듯.
+		//-> gameover() 이런식이 될듯.
+		ps = other.gameObject.GetComponent<Player_State>().getState();
 
+		if (ps.Equals(PlayerState.normal)) {
+			mon_state = 0;
+			Debug.Log ("GAME OVER");
+		} else {
+			Debug.Log (ps);
+		}
 
+	}
+
+	public int getMonState(){
+		return mon_state;
 	}
 
 }
---
+++ Doctor1_Move.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doctor1_Move : MonoBehaviour {
+
+
+	public float moveSpeed;	//움직이는 속도
+	public GameObject[] pointArr;	//목표지점 arr
+	public Transform objectToUse;	//game object-> 여기서는 캐릭터
+	public float[] distance;	//pointA, pointB 사이의 거리
+
+	private bool reverseMove;
+	private float startTime;	//pointA, pointB에서 부터 시작하는 시간
+	private float distCovered;	//속력
+	private float distPerTime;	//속력 / 거리
+	private int idx;
+
+	void Start()
+	{
+		idx = 0;
+
+		startTime = Time.time;	//start time
+		reverseMove = false;
+		for (int i = 0; i < 4 - 1; i++) {
+			distance [i] = Vector3.Distance (pointArr [i].transform.position, pointArr [i + 1].transform.position);
+		}
+
+	}
+	void Update()
+	{
+		distCovered = (Time.time - startTime) * moveSpeed;
+
+		if (!reverseMove) {
+			distPerTime = distCovered / distance[idx];
+			moveNextPoint (pointArr [idx], pointArr [getNextIdx (idx, reverseMove)]);
+			if (idx == 2 && (Vector3.Distance (objectToUse.position, pointArr[3].transform.position) == 0.0f)) {
+				reverseMove = true;
+				idx++;
+				startTime = Time.time;
+			}else if((Vector3.Distance (objectToUse.position, pointArr [getNextIdx (idx, reverseMove)].transform.position) == 0.0f)) {
+				idx = getNextIdx (idx, reverseMove);
+				startTime = Time.time;
+				Debug.Log ("Idx : " + idx);
+			}
+			transform.LookAt (pointArr [getNextIdx (idx, reverseMove)].transform);
+
+		} else {
+			distPerTime = distCovered / distance[idx-1];
+			moveNextPoint (pointArr [idx], pointArr [getNextIdx (idx, reverseMove)]);
+
+			if (idx == 1 && (Vector3.Distance (objectToUse.position, pointArr [0].transform.position) == 0.0f)) {
+				reverseMove = false;
+				idx--;
+				startTime = Time.time;
+
+			} else if ((Vector3.Distance (objectToUse.position, pointArr [getNextIdx (idx, reverseMove)].transform.position) == 0.0f)) {
+				idx = getNextIdx (idx, reverseMove);
+				startTime = Time.time;
+				Debug.Log ("rIdx : " + idx);
+			}
+			transform.LookAt (pointArr [getNextIdx (idx, reverseMove)].transform);
+
+		}
+
+	}
+
+	void moveNextPoint(GameObject a, GameObject b){
+		objectToUse.position = Vector3.Lerp (a.transform.position, b.transform.position, distPerTime);
+
+	}
+	int getNextIdx(int i, bool reverse){
+		if (!reverse)
+			return i + 1;
+		else
+			return i - 1;
+	}
+
+	public float getMoveSpeed(){
+		return moveSpeed;
+	}
+	public void setMoveSpeed(float spd){
+		moveSpeed = spd;
+	}
+}
