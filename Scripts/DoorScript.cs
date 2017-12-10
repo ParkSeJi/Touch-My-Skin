8c2e0ffd653fc7a23f954a47a392bf2daaf5ddad
kimdohyun14 <31722512+kimdohyun14@users.noreply.github.com>
2017-12-10 19:10:40 +0900

    Door

---
+++ Scripts/DoorScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    public bool open; //열렸는지 안 열렸는지
+    public float doorOpenAngle  = 170f; //열린 문의 각도
+    public float doorCloseAngle = 0f;   //닫힌 문의 각도
+    public float smooth = 2f;
+
+    public void ChangeDoorState() //문의 상태 변경
+    {
+        open = !open;
+        Debug.Log("open=!open");
+    }
+
+    void Update()
+    {
+        if(open)  // open == true 
+        {
+            Quaternion targetRotation = Quaternion.Euler(0,doorOpenAngle,0); //문의 y축 회전을 열린 문의 각도로 한다.
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smooth * Time.deltaTime);
+        }
+        else
+        {
+            Quaternion targetRotation2 = Quaternion.Euler(0, doorCloseAngle, 0);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation2, smooth * Time.deltaTime);
+        }
+    }
+}
---
+++ Scripts/InteractScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractScript : MonoBehaviour {
+
+
+    public float interactDistance = 5f; // 문과의 상호작용 간격(거리)
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) //left마우스버튼을 클릭했을 때 
+        {
+            Debug.Log("first if");
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;  //앞의 정보를 얻기 위해 Raycasthit사용
+            Debug.Log("Ray");
+            if (Physics.Raycast(ray,out hit,interactDistance))
+            {
+                Debug.Log("Second if");
+                if (hit.collider.CompareTag("Door")) // 태그에 부딪혔을 때
+                {
+                    hit.collider.transform.parent.GetComponent<DoorScript>().ChangeDoorState(); //부딪힌 대상의 부모 컴포넌트 취득하여 함수활용
+                    Debug.Log("third if");
+                }
+            }
+        }
+    }
+}
