2d35ca4995ed253b564654516ef1fd2b893035c5
BlockDMask <balxbalta@gmail.com>
2017-12-10 18:29:30 +0900

    171210

---
+++ Scripts/GameControll.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControll : MonoBehaviour {
+
+	public Player_State player;
+	public Text StateLabel;
+
+	void Update() //매 프레임 마다 플레이어의 상태를 text로 출력
+	{
+		StateLabel.text = "Player state: " + player.PS;
+	}
+}
