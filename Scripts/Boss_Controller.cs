dca3b91d09df300ec7286a416147ad78b89aca3e
BlockDMask <blockdmask@gmail.com>
2017-12-17 18:29:04 +0900

    Boss_move&대화

---
+++ Scripts/Boss_Controller.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BossState{
+	Idle, Move, Talk
+}
+
+
+public class Boss_Controller : MonoBehaviour {
+
+
+	public BossState BS;
+	public Animator animator;
+	// Use this for initialization
+	float Speed;
+	public float MoveSpeed;
+	public float FindRange = 20f;
+	public Transform Player;
+
+	void Start () {
+		Player = GameObject.Find ("Player").transform;
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (BS == BossState.Idle) {
+			DistanceCheck ();
+		} else if (BS == BossState.Move) {
+			MoveUpdate ();
+			TalkRangeCheck ();
+		} else if (BS == BossState.Talk) {
+			TalkRangeCheck ();
+		}
+	}
+
+	void DistanceCheck(){
+		if (Vector3.Distance (Player.position, this.transform.position) >= FindRange) {	//player가 범위 밖 일때
+			BS = BossState.Idle;
+			animator.SetBool ("walk", false);
+			Speed = 0;
+		} else {
+			BS = BossState.Move;
+			animator.SetBool ("walk", true);
+			Speed = MoveSpeed;
+		}
+	}
+
+	void MoveUpdate(){
+		transform.rotation = Quaternion.LookRotation (
+			new Vector3 (Player.position.x, this.transform.position.y, Player.position.z)- transform.position);
+		transform.Translate (Vector3.forward * Speed * Time.deltaTime);
+	}
+
+	void TalkRangeCheck(){
+		if (Vector3.Distance (Player.position, this.transform.position) < 2f) {
+			Speed = 0;
+			BS = BossState.Talk;
+		} else {
+			Speed = MoveSpeed;
+			BS = BossState.Move;
+		}
+	}
+
+}
---
+++ Scripts/Boss_Trigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boss_Trigger : MonoBehaviour {
+
+	public GameObject canvas_talk;
+	public GameObject canvas_main;
+	public GameObject talkObject;
+	private DialogueManager dialog;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		dialog = talkObject.GetComponent<DialogueManager> ();
+		if (other.tag == "Player") {
+			Debug.Log ("boss_trigger : enter");
+			other.GetComponent<PlayerMoveController> ().setMoveSpeed (0.0f);
+			canvas_main.SetActive (false);
+			canvas_talk.SetActive (true);
+			dialog.StartDialogue ();
+		}
+	}
+
+	void OnTriggerStay(Collider other){
+		if (other.tag == "Player" && dialog.isEnd) {
+			Debug.Log ("boss_trigge : stay");
+			other.GetComponent<PlayerMoveController> ().setMoveSpeed (4.0f);
+			canvas_main.SetActive (true);
+			canvas_talk.SetActive (false);
+		}
+	}
+
+}
